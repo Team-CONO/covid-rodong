27eea8ec691b95ba4ffae9f3038e5c6f7473cc5a
Kim HyunWoo <stories2@naver.com>
2020-05-01 16:57:53 +0900

    test aos

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
+    <h1 data-aos="fade">{{ msg }}</h1>
+    <h2 data-aos="flip-up" data-aos-delay="300">Essential Links</h2>
+    <ul data-aos="flip-up" data-aos-delay="600">
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
+    <div style="height:900px"></div>
+    <h2 data-aos="slide-up" data-aos-delay="300" data-aos-offset="400">Ecosystem</h2>
+    <ul data-aos="slide-up" data-aos-delay="300" data-aos-offset="400">
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <div style="height:900px"></div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 new Vue({
   el: '#app',
+  created() {
+    AOS.init({
+      // duration: 3000
+    });
+  },
   render: h => h(App)
 })
