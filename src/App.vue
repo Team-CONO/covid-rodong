aa28e81d765f6483962f7fa3cc19ea642ba371d8
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-05-15 21:57:54 +0900

    Darkmode 설정
    기본색으론 이게 괜찮?

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+body{
+  background-color: #171E29;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueDarkMode from "@growthbunker/vuedarkmode"
 
 Vue.config.productionTip = false
+Vue.use(VueDarkMode);
 
 new Vue({
   render: h => h(App),
