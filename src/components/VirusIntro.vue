f1850d1754dd89538d34317891f3454f3d44dc2c
Kim HyunWoo <stories2@naver.com>
2020-06-07 20:46:19 +0900

    컴포넌트 추가

---
+++ src/components/VirusIntro.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  HELLTAKER
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VirusIntro',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+</style>
