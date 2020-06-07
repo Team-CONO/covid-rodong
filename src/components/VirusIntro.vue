be6e55888e24e09112b41213419132672cddf039
Kim HyunWoo <stories2@naver.com>
2020-06-07 22:39:13 +0900

    강조 추가

--- src/components/VirusIntro.vue
+++ src/components/VirusIntro.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" class="covid">
     <div class="intro">
-        <h2>코로나 바이러스란?</h2>
+        <h2><span class="focus">코로나</span> 바이러스란?</h2>
     </div>
-    <div class="info">코로나바이러스과에 속하는 RNA 바이러스</div>
-    <div class="info">사람과 동물의 호흡기와 소화기계 감염을 유발하며</div>
-    <div class="info">점막전염 혹은 비말전파로 쉽게 전파된다</div>
+    <div class="info">코로나바이러스과에 속하는 <span class="focus">RNA 바이러스</span></div>
+    <div class="info">사람과 동물의 <span class="focus">호흡기</span>와 <span class="focus">소화기계</span> 감염을 유발하며</div>
+    <div class="info"><span class="focus">점막전염</span> 혹은 <span class="focus">비말전파</span>로 쉽게 전파된다</div>
   </div>
 </template>
 
@@ -19,6 +19,10 @@ export default {
 </script>
 
 <style>
+span.focus {
+    color: red;
+    font-weight: bold;
+}
 div.info {
     text-align:center;
     color:white;
