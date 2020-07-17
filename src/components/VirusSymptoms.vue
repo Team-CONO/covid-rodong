b9c12b5214b4c658129e5d73069265d0e877ae69
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-07-18 07:38:27 +0900

    바이러스 증상 추가
    아직 미완성 상태

---
+++ src/components/VirusSymptoms.vue
@@ -0,0 +1,37 @@
+<template>
+  <article>
+    <section id="up" data-aos="fade-down">
+      <img src="../assets/icon-screening-cough.png" alt="a img">      
+    </section>
+    <section id="up" data-aos="fade-down">
+      <img src="../assets/icon-screening-fever-chills.png" alt="b img">      
+    </section>
+    <section id="up" data-aos="fade-down">
+      <img src="../assets/icon-screening-headache.png" alt="c img">      
+    </section>
+    <section id="up" data-aos='fade-up'>
+      <img src="../assets/icon-screening-musclepain.png" alt="d img">      
+    </section>
+    <section id="up" data-aos='fade-up'>
+      <img src="../assets/icon-screening-senses.png" alt="e img">      
+    </section>
+    <section id="up" data-aos='fade-up'>
+      <img src="../assets/icon-screening-sorethroat.png" alt="f img">      
+    </section>
+  </article>
+</template>
+
+<style>
+  #up {
+    float: left;
+    margin-left: 3rem;
+  }
+  img {
+    width: 500px; 
+    height: 500px;
+    opacity: 0.5;
+  }
+  img:hover {
+    opacity: 1.0;
+  }
+</style>
