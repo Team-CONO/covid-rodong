c6051c31fdcfc2b46c7583a2242a61597c09e199
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-07-18 14:49:24 +0900

    바이러스 증상 완성
    p 태그 css 값 조절이 조금은 더 필요한?

--- src/components/VirusSymptoms.vue
+++ src/components/VirusSymptoms.vue
@@ -1,37 +1,49 @@
 <template>
   <article>
-    <section id="up" data-aos="fade-down">
-      <img src="../assets/icon-screening-cough.png" alt="a img">      
+    <section id="left" data-aos="fade-down">
+      <img class="img" src="../assets/icon-screening-cough.png" alt="a img">
+      <p class="text">기침, 마른기침</p>
     </section>
-    <section id="up" data-aos="fade-down">
-      <img src="../assets/icon-screening-fever-chills.png" alt="b img">      
+    <section id="left" data-aos="fade-down">
+      <img class="img" src="../assets/icon-screening-fever-chills.png" alt="b img">
+      <p class="text">발열 또는 오한</p>
     </section>
-    <section id="up" data-aos="fade-down">
-      <img src="../assets/icon-screening-headache.png" alt="c img">      
+    <section id="left" data-aos="fade-down">
+      <img class="img" src="../assets/icon-screening-headache.png" alt="c img">
+      <p class="text">두통</p>
     </section>
-    <section id="up" data-aos='fade-up'>
-      <img src="../assets/icon-screening-musclepain.png" alt="d img">      
+    <section id="left" data-aos='fade-up'>
+      <img class="img" src="../assets/icon-screening-musclepain.png" alt="d img">
+      <p class="text">근육통 또는 몸살</p>
     </section>
-    <section id="up" data-aos='fade-up'>
-      <img src="../assets/icon-screening-senses.png" alt="e img">      
+    <section id="left" data-aos='fade-up'>
+      <img class="img" src="../assets/icon-screening-senses.png" alt="e img">
+      <p class="text">미각 또는 후각 상실</p>
     </section>
-    <section id="up" data-aos='fade-up'>
-      <img src="../assets/icon-screening-sorethroat.png" alt="f img">      
+    <section id="left" data-aos='fade-up'>
+      <img class="img" src="../assets/icon-screening-sorethroat.png" alt="f img">
+      <p class="text">목 아픔</p>
     </section>
   </article>
 </template>
 
 <style>
-  #up {
+  #left {
     float: left;
-    margin-left: 3rem;
+    margin-left: 6rem;
   }
-  img {
+  #left .text {
+    visibility: hidden;
+  }
+  #left .img {
     width: 500px; 
     height: 500px;
     opacity: 0.5;
   }
-  img:hover {
+  #left:hover .img {
     opacity: 1.0;
+  }
+  #left:hover .text {
+    visibility: visible;
   }
 </style>
