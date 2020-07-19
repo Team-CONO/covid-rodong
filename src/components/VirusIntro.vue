ecb6010c74861294352f1e0238c461bde5752e6f
Kim HyunWoo <stories2@naver.com>
2020-07-19 11:23:21 +0900

    add aos anim

--- src/components/VirusIntro.vue
+++ src/components/VirusIntro.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" class="covid">
-    <div class="intro">
+    <div class="intro" data-aos="zoom-out">
         <h2><span class="focus">코로나</span> 바이러스란?</h2>
     </div>
-    <div class="info">코로나바이러스과에 속하는 <span class="focus">RNA 바이러스</span></div>
-    <div class="info">사람과 동물의 <span class="focus">호흡기</span>와 <span class="focus">소화기계</span> 감염을 유발하며</div>
-    <div class="info"><span class="focus">점막전염</span> 혹은 <span class="focus">비말전파</span>로 쉽게 전파된다</div>
+    <div class="info" data-aos="fade-up">코로나바이러스과에 속하는 <span class="focus">RNA 바이러스</span></div>
+    <div class="info" data-aos="fade-up" data-aos-delay="50">사람과 동물의 <span class="focus">호흡기</span>와 <span class="focus">소화기계</span> 감염을 유발하며</div>
+    <div class="info" data-aos="fade-up" data-aos-delay="100"><span class="focus">점막전염</span> 혹은 <span class="focus">비말전파</span>로 쉽게 전파된다</div>
   </div>
 </template>
 
